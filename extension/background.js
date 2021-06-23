0cada66d560ea6c432b3b90e50bc97ff9668e3bb
Adrien Lacquemant <github@alaq.io>
2021-06-23 00:08:30 -0400

    Proof of concept of side by side searches

---
+++ extension/background.js
@@ -0,0 +1,13 @@
+const HEADERS_TO_STRIP_LOWERCASE = ["content-security-policy", "x-frame-options"];
+
+chrome.webRequest.onHeadersReceived.addListener(
+    (details) => ({
+        responseHeaders: details.responseHeaders.filter(
+            (header) => !HEADERS_TO_STRIP_LOWERCASE.includes(header.name.toLowerCase())
+        ),
+    }),
+    {
+        urls: ["<all_urls>"],
+    },
+    ["blocking", "responseHeaders"]
+);
