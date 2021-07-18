c9c653c11f48e8cac5b9a2cf22e2c696784b830d
Adrien Lacquemant <github@alaq.io>
2021-07-18 02:07:23 -0400

    Prepare for custom opensearch

--- frontend/api/opensearch.js
+++ frontend/api/opensearch.js
@@ -1,40 +1,24 @@
 module.exports = (req, res) => {
     res.setHeader("content-type", "application/opensearchdescription+xml");
-    res.write(createXMLString());
+    res.write(createXMLString(req.query.left || "Brave", req.query.right || "Google"));
     res.end();
 };
 
-function createXMLString() {
-    // const parser = new DOMParser();
-    // const doc = parser.parseFromString(XML_TEMPLATE, "application/xml");
+const suggestionMap = {
+    Brave: "https://search.brave.com/api/suggest?q={searchTerms}",
+    Google: "https://search.brave.com/api/suggest?q={searchTerms}",
+    DuckDuckGo: "https://search.brave.com/api/suggest?q={searchTerms}",
+    Startpage: "https://search.brave.com/api/suggest?q={searchTerms}",
+};
 
-    // // Search URL
-    // const url = doc.querySelector('Url[type="text/html"]');
-    // // url.setAttribute("template", browser.extension.getURL("search.html?q=") + "{searchTerms}");
-    // url.setAttribute("template", "https://google.com/search?q={searchTerms}");
-    // url.setAttribute("method", "GET");
-
-    // const image = doc.getElementsByTagName("Image")[0];
-    // image.textContent = "https://side-by-side-search.vercel.app/images/icon64.png";
-
-    // const suggestUrl = doc.querySelector('Url[type="application/x-suggestions+json"]');
-    // suggestUrl.setAttribute("method", "GET");
-    // suggestUrl.setAttribute("template", "https://search.brave.com/api/suggest?q={searchTerms}");
-
-    // // Description
-    // const description = doc.getElementsByTagName("Description")[0];
-    // description.textContent = "Side-by-side comparison of your favorite search engines";
-
-    // const serialzer = new XMLSerializer();
-    // const string = serialzer.serializeToString(doc);
-
+function createXMLString(left, right) {
     return `<?xml version="1.0" encoding="UTF-8"?>
     <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
     <ShortName>Side-by-side Search</ShortName>
     <Description>Side-by-side comparison of your favorite search engines</Description>
     <InputEncoding>UTF-8</InputEncoding>
     <Image height="16" width="16">https://side-by-side-search.vercel.app/images/icon64.png</Image>
-    <Url type="text/html" method="GET" template="https://google.com/search?q={searchTerms}"/>
+    <Url type="text/html" method="GET" template="https://side-by-side-search.vercel.app/search.html?q={searchTerm}&left=Brave&right=Google"/>
     <Url type="application/x-suggestions+json" method="GET" template="https://search.brave.com/api/suggest?q={searchTerms}"/>
     </OpenSearchDescription>`;
 }
