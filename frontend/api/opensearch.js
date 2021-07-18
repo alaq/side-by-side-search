b70cb5b0cb44d7efae3a61dc0c00c07dada26f65
Adrien Lacquemant <github@alaq.io>
2021-07-18 02:32:04 -0400

    Declare a variable before returning in API

--- frontend/api/opensearch.js
+++ frontend/api/opensearch.js
@@ -12,13 +12,14 @@ const suggestionMap = {
 };
 
 function createXMLString(left, right) {
-    return `<?xml version="1.0" encoding="UTF-8"?>
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
     <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
     <ShortName>Side-by-side Search</ShortName>
     <Description>Side-by-side comparison of your favorite search engines</Description>
     <InputEncoding>UTF-8</InputEncoding>
     <Image height="16" width="16">https://side-by-side-search.vercel.app/images/icon64.png</Image>
-    <Url type="text/html" method="GET" template="https://side-by-side-search.vercel.app/search.html?q={searchTerm}&left=Brave&right=Google"/>
+    <Url type="text/html" method="GET" template="https://side-by-side-search.vercel.app/search.html?q={searchTerm}&left=${left}&right=${right}"/>
     <Url type="application/x-suggestions+json" method="GET" template="https://search.brave.com/api/suggest?q={searchTerms}"/>
     </OpenSearchDescription>`;
+    return xml;
 }
