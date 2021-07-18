2f46390f69f51cc01b31881750c7be3af922c9b8
Adrien Lacquemant <github@alaq.io>
2021-07-18 18:42:36 -0400

    Use settings to customize the opensearch search engine

--- frontend/index.js
+++ frontend/index.js
@@ -3,13 +3,21 @@ const savePreferences = () => {
         left: document.getElementById("left").value,
         right: document.getElementById("right").value,
     };
+    setOpensearchUrl(enginePreferences);
     localStorage.setItem("engines", JSON.stringify(enginePreferences));
 };
 
 const loadPreferences = () => {
     const engines = JSON.parse(localStorage.getItem("engines"));
+    setOpensearchUrl(engines);
     document.getElementById("left").value = engines.left;
     document.getElementById("right").value = engines.right;
+};
+
+const setOpensearchUrl = ({ left, right }) => {
+    document.querySelector(
+        "link[rel=search]"
+    ).href = `https://side-by-side-search.vercel.app/api/opensearch.xml?left=${left}&right=${right}`;
 };
 
 window.onload = () => {
