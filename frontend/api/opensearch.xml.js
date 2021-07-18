a65aab7ce8ba1e95505e2cb3dcb8463f56ae759c
Adrien Lacquemant <github@alaq.io>
2021-07-18 02:57:33 -0400

    Remove space in front of opensearch XML

--- frontend/api/opensearch.xml.js
+++ frontend/api/opensearch.xml.js
@@ -13,13 +13,13 @@ const suggestionMap = {
 
 function createXMLString(left, right) {
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
-    <ShortName>Side-by-side Search</ShortName>
-    <Description>Side-by-side comparison of your favorite search engines</Description>
-    <InputEncoding>UTF-8</InputEncoding>
-    <Image height="16" width="16">https://side-by-side-search.vercel.app/images/icon64.png</Image>
-    <Url type="text/html" method="GET" template="https://side-by-side-search.vercel.app/search.html?q={searchTerm}&left=${left}&right=${right}"/>
-    <Url type="application/x-suggestions+json" method="GET" template="${suggestionMap[left]}"/>
-    </OpenSearchDescription>`;
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+<ShortName>Side-by-side Search</ShortName>
+<Description>Side-by-side comparison of your favorite search engines</Description>
+<InputEncoding>UTF-8</InputEncoding>
+<Image height="16" width="16">https://side-by-side-search.vercel.app/images/icon64.png</Image>
+<Url type="text/html" method="GET" template="https://side-by-side-search.vercel.app/search.html?q={searchTerm}&left=${left}&right=${right}"/>
+<Url type="application/x-suggestions+json" method="GET" template="${suggestionMap[left]}"/>
+</OpenSearchDescription>`;
     return xml;
 }
