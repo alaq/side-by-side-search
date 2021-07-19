ac8420a3970c66c10fde6737de41737c41086e73
Adrien Lacquemant <github@alaq.io>
2021-07-18 20:14:30 -0400

    Add notification and let it be hidden

--- extension/extension-content-only.js
+++ extension/extension-content-only.js
@@ -1,1 +1,4 @@
 document.getElementsByTagName("form")[0].classList.remove("hidden");
+if (!localStorage.getItem("hideInstallationInstructions")) {
+    document.getElementById("installation-notification").classList.remove("hidden");
+}
--- frontend/index.js
+++ frontend/index.js
@@ -9,15 +9,22 @@ const savePreferences = () => {
 
 const loadPreferences = () => {
     const engines = JSON.parse(localStorage.getItem("engines"));
-    setOpensearchUrl(engines);
-    document.getElementById("left").value = engines.left;
-    document.getElementById("right").value = engines.right;
+    if (engines) {
+        setOpensearchUrl(engines);
+        document.getElementById("left").value = engines.left;
+        document.getElementById("right").value = engines.right;
+    }
 };
 
-const setOpensearchUrl = ({ left, right }) => {
+const setOpensearchUrl = ({ left = "Brave", right = "Google" }) => {
     document.querySelector(
         "link[rel=search]"
     ).href = `https://side-by-side-search.vercel.app/api/opensearch.xml?left=${left}&right=${right}`;
+};
+
+const hideInstructions = () => {
+    localStorage.setItem("hideInstallationInstructions", true);
+    document.getElementById("installation-notification").classList.add("hidden");
 };
 
 window.onload = () => {
