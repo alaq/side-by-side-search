c6c4ee972b4da11a7cd423642de8961f35655b75
Adrien Lacquemant <github@alaq.io>
2021-07-18 21:00:42 -0400

    Open search engine on install or update

--- extension/background.js
+++ extension/background.js
@@ -11,3 +11,39 @@ chrome.webRequest.onHeadersReceived.addListener(
     },
     ["blocking", "responseHeaders"]
 );
+
+chrome.runtime.onInstalled.addListener((details) => {
+    if (details.reason === "install" || details.reason === "update") {
+        refreshBrowser("gmail-inbox", true);
+    }
+});
+
+// This is taken from https://www.gmass.co/blog/redirect-user-to-website-after-installing-chrome-extension/
+function refreshBrowser(target, bringToForeground) {
+    if (target !== "gmail-inbox") return;
+    chrome.windows.getAll({ populate: true }, (windows) => {
+        let foundExisting = false;
+        windows.forEach((win) => {
+            win.tabs.forEach((tab) => {
+                // Ignore tabs not matching the target.
+                if (target === "gmail-inbox") {
+                    if (!/https:\/\/side-by-side-search\.vercel\.app/.test(tab.url)) return;
+                } else {
+                    return; // Unknown target.
+                }
+                // Reload the matching tab.
+                chrome.tabs.reload(tab.id); // If this is the first one found, activate it.
+                if (bringToForeground && !foundExisting) {
+                    chrome.tabs.update(tab.id, { active: true });
+                }
+                foundExisting = true;
+            });
+        });
+        if (bringToForeground && !foundExisting) {
+            chrome.tabs.create({
+                url: "https://side-by-side-search.vercel.app/",
+                active: true,
+            });
+        }
+    });
+}
