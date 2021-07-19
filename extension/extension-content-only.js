433dcffa0d2d03c5fcaf29913a084bd77f4cdaba
Adrien Lacquemant <github@alaq.io>
2021-07-18 20:49:24 -0400

    Add images for browser extensions

--- extension/extension-content-only.js
+++ extension/extension-content-only.js
@@ -1,4 +1,5 @@
 document.getElementsByTagName("form")[0].classList.remove("hidden");
+Array.from(document.getElementsByClassName("extension-stores")).forEach((image) => image.classList.add("hidden"));
 if (!localStorage.getItem("hideInstallationInstructions")) {
     document.getElementById("installation-notification").classList.remove("hidden");
 }
